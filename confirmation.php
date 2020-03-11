a428c550e8f8c7a445d1e01e06eec01777479284
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-03-11 13:24:54 +0000

    Added pound symbol (£)

--- confirmation.php
+++ confirmation.php
@@ -7,5 +7,5 @@ CO551 Open Source Systems -->
 
     echo "<h2> Your order quantity is " . $_SESSION['quantity'] . "</h2></br>";
     echo "<h2> and the selected colour is " . $_POST['selcolour'] . "</h2>";
-    echo "<h2> The total price of your widgets is " . $_SESSION['totalcost'] ."</h2>";
+    echo "<h2> The total price of your widgets is £" . $_SESSION['totalcost'] ."</h2>";
 ?>
