4067e1cce97f4f03b8b7d6f29a333b051df35605
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-03-11 13:18:16 +0000

    Completed Unit 4 Exercise 2 plus started Exercise 3

--- confirmation.php
+++ confirmation.php
@@ -1,7 +1,11 @@
 <!-- Author: Ana Lucia Petinga Zorro
 CO551 Open Source Systems -->
+<?php session_start(); ?>
 <?php
-    session_start();
+    $_SESSION['price'] = txtprice; 
+    $_SESSION['totalcost'] = $_SESSION['quantity'] * $_SESSION['price'];
+
     echo "<h2> Your order quantity is " . $_SESSION['quantity'] . "</h2></br>";
     echo "<h2> and the selected colour is " . $_POST['selcolour'] . "</h2>";
+    echo "<h2> The total price of your widgets is " . $_SESSION['totalcost'] ."</h2>";
 ?>
--- size.php
+++ size.php
@@ -1,2 +1,20 @@
 <!-- Author: Ana Lucia Petinga Zorro
 CO551 Open Source Systems -->
+<html>
+    <head>
+        <title>Select size page</title>
+    </head>
+    <body>
+        <form action="selectcolour.php" method="post">
+            Select the size for the <?php echo $_SESSION['quantity']  ?> widgets you are ordering:
+            <select name="selsize">
+                <option>Small</option>
+                <option>Medium</option>
+                <option>Large</option>
+                <option>Extra Large</option>
+            </select>
+            <br/><br/>	
+            <input type="submit" value="Buy"/>
+        </form>
+    </body>
+</html>
