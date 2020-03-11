ff185592649dbce9d362550094666686c348939f
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-03-11 10:43:11 +0000

    minor change

--- selectcolour.php
+++ selectcolour.php
@@ -8,7 +8,7 @@ CO551 Open Source Systems -->
   </head>
   <body>
     <form action="confirmation.php"  method="post">
-      Select the colour for the <?php echo $_POST[selqty] ?> widgets you are ordering
+      Select the colour for the <?php echo $_SESSION['quantity']  ?> widgets you are ordering
       <select name="selcolour">
         <option>white</option>
         <option>red</option>
