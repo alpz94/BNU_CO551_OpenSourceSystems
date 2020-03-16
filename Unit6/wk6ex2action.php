0bec6f017b125192d677ae50393be799143905d5
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-03-16 16:50:55 +0000

    moved ex2 action into folder

--- Unit6/wk6ex2action.php
+++ Unit6/wk6ex2action.php
@@ -2,22 +2,22 @@
 CO551 Open Source Systems -->
 <?php
     $link = mysqli_connect("localhost", "root", "", "test");
-	
+
 	$sql = "SELECT * from test where name = '$_GET[id]' ";
 	$result = mysqli_query($link, $sql);
-	$row = mysqli_fetch_assoc($result);
+    $row = mysqli_fetch_assoc($result);
 ?>
 <html>
     <body>
     <form action="" method="post">
         Name :
-        <input type=text name=txtname value="<? echo $row['name'] ?>" readonly />
+        <input type=text name=txtname value="<?php echo $row['name'] ?>" readonly />
         </br>
         Phone number :
-        <input type=text name=txttelno value="<? echo $row['phone_number'] ?>" />
+        <input type=text name=txttelno value="<?php echo $row['phone_number'] ?>" />
         </br>
         Email :
-        <input type=text name=txtemail value="<? echo $row['email'] ?>" />
+        <input type=text name=txtemail value="<?php echo $row['email'] ?>" />
         </br>
         <input type=submit name=btnsubmit value="save"/>
     </form>
