ab4bb6a153558fc4f1aedf841e22fcddc2c587d7
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-03-16 14:11:02 +0000

    c

--- Unit6/wk6ex1action.php
+++ Unit6/wk6ex1action.php
@@ -4,13 +4,13 @@ CO551 Open Source Systems -->
 	$sql = "INSERT INTO test (name,email,phone_number) ";
 	$sql = $sql . " values ('$_POST[txtName]','$_POST[txtEmail]','$_POST[txtPhoneNumber]')";
 
-    $link = mysqli_connect("localhost", "root", "", "test");
+	$link = mysqli_connect("localhost", "root", "", "test");
 	
-    $result = mysqli_query($link, $sql);
+	$result = mysqli_query($link, $sql);
 
 	$sql = "SELECT * from test";
 	
-    $result = mysqli_query($link, $sql);
+	$result = mysqli_query($link, $sql);
     
 	while ($row = mysqli_fetch_assoc($result))
 	{
