322a3eb907b9e92056daaec8585523256c8187e3
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-03-16 15:14:15 +0000

    Unit 6 Exercise 2

---
+++ Unit6/wk6ex2.php
@@ -0,0 +1,17 @@
+<!-- Author: Ana Lucia Petinga Zorro
+CO551 Open Source Systems -->
+<?php
+    $link = mysqli_connect("localhost", "root", "", "test");
+	$sql = "SELECT * from test";
+	$result = mysqli_query($link, $sql);
+?>
+<html>
+    <body>
+        <?php
+            while ($row = mysqli_fetch_assoc($result))
+            {
+                echo "<a href=\"wk6ex2action.php?id=$row[name]\">$row[name]</a></br>";  	
+            }
+        ?>
+    </body>
+</html>
