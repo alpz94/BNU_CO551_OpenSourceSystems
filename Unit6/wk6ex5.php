408e33c9305b301010652a13f7cb360b3cbf505d
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-03-18 21:00:20 +0000

    added code

--- Unit6/wk6ex5.php
+++ Unit6/wk6ex5.php
@@ -1,5 +1,8 @@
 <!-- Author: Ana Lucia Petinga Zorro
 CO551 Open Source Systems -->
 <?php
-
+    include(“myfunctions.inc”);
+    html_header(“My first function demo”);
+    html_h1(“These functions are going to save me lots of time”);
+    html_footer();
 ?>
