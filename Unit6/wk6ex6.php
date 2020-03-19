f46a92e61af5c132905d33a3ad685ecde0b701ce
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-03-19 00:59:34 +0000

    Moved into Unit 6 folder

--- Unit6/wk6ex6.php
+++ Unit6/wk6ex6.php
@@ -3,6 +3,6 @@ CO551 Open Source Systems -->
 <?php
 	include("myfunctions.inc");
 	html_header("My second function demo");
-	echo "I pay £" . calculatetax(15000,22) . " tax";
+	echo "I pay £" . calculatetax(7413,22) . " tax";
 	html_footer();
 ?>
