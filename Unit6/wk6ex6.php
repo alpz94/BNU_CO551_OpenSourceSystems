58e3fbdb318ca08fbd587703ce456c822d5c0c20
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-03-30 14:46:48 +0100

    Took into account tax allowance

--- Unit6/wk6ex6.php
+++ Unit6/wk6ex6.php
@@ -3,6 +3,6 @@ CO551 Open Source Systems -->
 <?php
 	include("myfunctions.inc");
 	html_header("My second function demo");
-	echo "I pay £" . calculatetax(7413) . " tax";
+	echo "I pay £" . calculatetax(7413) . " tax, being that my tax allowance is $allowance";
 	html_footer();
 ?>
