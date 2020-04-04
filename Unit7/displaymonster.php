f54876db54ef80669854f41cd570669bdd8b9683
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-04-04 20:47:03 +0100

    added code

--- Unit7/displaymonster.php
+++ Unit7/displaymonster.php
@@ -1,2 +1,14 @@
 <!-- Author: Ana Lucia Petinga Zorro
 CO551 Open Source Systems -->
+<?php
+    $conn = mysqli_connect("localhost", "21903356", "mysqluser", "db5_21903356");
+
+    $sql = "select id from monster;";
+
+    $result = mysqli_query($sql, $db);
+    $row = mysqli_fetch_array($result);
+
+    echo "<img src='getjpg.php?id=" . $row[id]. "'/>";
+
+    mysqli_close();
+?>
