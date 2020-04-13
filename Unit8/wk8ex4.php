90504ab5422b173726a32b9c1a080be474380b79
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-04-13 14:54:56 +0100

    exercise 4 part 1

--- Unit8/wk8ex4.php
+++ Unit8/wk8ex4.php
@@ -1,17 +1,21 @@
 <!-- Author: Ana Lucia Petinga Zorro
 CO551 Open Source Systems -->
-<?php     
-     $conn = mysqli_connect("localhost", username, password, databasename);
-       $sql = "select * from lotto;";
-       $result = mysqli_query($sql);
+<?php
+    session_start();
 
-       echo "<form action='$_SESSION[PHP_SELF]' method='post' >";
-       echo "<br/>Select the lottery week ";
-       echo "<select name='selweek' >";
-       while($row = mysqli_fetch_array($result)) {
-  	  echo "<option value='$row[wk]'>$row[wk]</option>";
-       }
-       echo "</select><br/>";
-       echo "<input type='submit' value='Select' />";
-       echo "</form>";
+    $conn = mysqli_connect("localhost", "root", "", "db_week8");
+
+    $sql = "select * from lotto;";
+    $result = mysqli_query($conn, $sql);
+
+    echo "<form action='$_SESSION[PHP_SELF]' method='post' >";
+    echo "<br/>Select the lottery week ";
+    echo "<select name='selweek' >";
+    while($row = mysqli_fetch_array($result))
+    {
+        echo "<option value='$row[wk]'>$row[wk]</option>";
+    }
+    echo "</select><br/>";
+    echo "<input type='submit' value='Select' />";
+    echo "</form>";
 ?>
