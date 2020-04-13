8ad3858367ef46a005d77c82016375491c65eda6
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-04-13 14:42:58 +0100

    change dmY to Ymd

--- Unit8/wk8ex1.php
+++ Unit8/wk8ex1.php
@@ -1,7 +1,7 @@
 <!-- Author: Ana Lucia Petinga Zorro
 CO551 Open Source Systems -->
 <?php
-     $lottodate = date("dmY");
+     $lottodate = date("Ymd");
      echo "The lottery numbers for $lottodate are ";
      for($n=1;$n<7;$n++)
      {
@@ -16,6 +16,6 @@ CO551 Open Source Systems -->
      $sql = $sql . " values ($lottodate,$number[1],$number[2],";
      $sql = $sql . "$number[3],$number[4],$number[5],$number[6]);";
 
-     $result = mysqli_query($sql);
+     $result = mysqli_query($conn, $sql);
      echo "<br/>This weeks numbers have been saved";
 ?>
