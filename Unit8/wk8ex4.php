7e6a4df86cb3b72a0931fe06720d279d6630aec8
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-04-13 21:03:42 +0100

    Exercise 4 part 2 complete

--- Unit8/wk8ex4.php
+++ Unit8/wk8ex4.php
@@ -1,29 +1,45 @@
 <!-- Author: Ana Lucia Petinga Zorro
 CO551 Open Source Systems -->
-<?php session_start();?>
-<?php
-
-    $conn = mysqli_connect("localhost", "root", "", "db_week8");
-
-    $sql = "select * from lotto;";
-    $result = mysqli_query($conn, $sql);
-?>
 <html>
     <head>
-        <h2>Exercise 4: lotto table, db_week8 database</h2>
+        <h2>Exercise 4 part 2: lotto table, db_week8 database</h2>
     </head>
-    <body>
-        <form action="<?php $_SESSION["PHP_SELF"];?>" method="post">
-            <?php    
-                echo "<br/>Select the lottery week ";
-                echo "<select name='selweek' >";
-                while($row = mysqli_fetch_array($result))
-                {
-                    echo "<option value='$row[wk]'>$row[wk]</option>";
-                }
-                echo "</select><br/>";
-                echo "<input type='submit' value='Select' />";
-            ?>
-        </form>
-    </body>
 </html>
+
+<?php 
+    session_start();
+
+    $conn = mysqli_connect("localhost", "root", "", "db_week8");
+    
+    if (isset($_POST['selweek']))
+    {
+        $sql = "SELECT * FROM lotto WHERE wk=$_POST[selweek];";
+        $result = mysqli_query($conn, $sql);
+        $row =mysqli_fetch_array($result);
+        
+        echo "Number 1 is  $row[number1]<br/>";
+        echo "Number 2 is  $row[number2]<br/>";
+        echo "Number 3 is  $row[number3]<br/>";
+        echo "Number 4 is  $row[number4]<br/>";
+        echo "Number 5 is  $row[number5]<br/>";
+        echo "Number 6 is  $row[number6]<br/>";
+    }
+    else
+    {
+        $sql = "SELECT * FROM lotto";
+        $result = mysqli_query($conn, $sql);
+
+        echo "<form action='' method='post' >";
+        echo "<br/>Select the lottery week ";
+        echo "<select name='selweek' >";
+
+        while($row = mysqli_fetch_array($result))
+        {
+            echo "<option value='$row[wk]'>$row[wk]</option>";
+        }
+        
+        echo "</select><br/>";
+        echo "<input type='submit' value='Select' />";
+        echo "</form>";
+   }
+?>
