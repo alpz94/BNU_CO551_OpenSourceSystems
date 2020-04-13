3a481c80506c66ce2192ae9984d4114d868c2b92
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-04-13 15:38:25 +0100

    added html tags arranged code

--- Unit8/wk8ex4.php
+++ Unit8/wk8ex4.php
@@ -1,21 +1,29 @@
 <!-- Author: Ana Lucia Petinga Zorro
 CO551 Open Source Systems -->
+<?php session_start();?>
 <?php
-    session_start();
 
     $conn = mysqli_connect("localhost", "root", "", "db_week8");
 
     $sql = "select * from lotto;";
     $result = mysqli_query($conn, $sql);
-
-    echo "<form action='$_SESSION[PHP_SELF]' method='post' >";
-    echo "<br/>Select the lottery week ";
-    echo "<select name='selweek' >";
-    while($row = mysqli_fetch_array($result))
-    {
-        echo "<option value='$row[wk]'>$row[wk]</option>";
-    }
-    echo "</select><br/>";
-    echo "<input type='submit' value='Select' />";
-    echo "</form>";
 ?>
+<html>
+    <head>
+        <h2>Exercise 4: lotto table, db_week8 database</h2>
+    </head>
+    <body>
+        <form action="<?php $_SESSION["PHP_SELF"];?>" method="post">
+            <?php    
+                echo "<br/>Select the lottery week ";
+                echo "<select name='selweek' >";
+                while($row = mysqli_fetch_array($result))
+                {
+                    echo "<option value='$row[wk]'>$row[wk]</option>";
+                }
+                echo "</select><br/>";
+                echo "<input type='submit' value='Select' />";
+            ?>
+        </form>
+    </body>
+</html>
